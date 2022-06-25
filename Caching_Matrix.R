003c24f15ad2bef3cbb5e9c139a2eb69c75cb19a
Alexander Hoffmann <106266229+AlexH1998@users.noreply.github.com>
2022-06-25 19:11:42 +0200

    Add files via upload

---
+++ Caching_Matrix.R
@@ -0,0 +1,31 @@
+
+makeCacheMatrix <- function( m = matrix()){
+    inv <- NULL
+    set <- function(matrix){
+        x <<- matrix
+        inv <<- NULL
+    }
+    get <- function(){
+        x
+    }
+    
+    setinv <- function(inverse){
+        inv <<- inverse
+    }
+    getinv <- function(){
+        inv
+    }
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+cacheSolve <- function(x, ...){
+    inv <- x$getinv()
+    if (!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat) %*% mat
+    x$setinv(inv)
+    inv
+}
